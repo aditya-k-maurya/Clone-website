bcbe26aa1d15d469ac79a01f0adf774b68a7acdf
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2023-10-01 13:43:46 +0530

    nav responsive

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const showMenuBtn = document.getElementById("show-menu");
-        let navLinks = document.querySelector(".nav-links");
-
+const navLinks = document.getElementById("nav-links");
+const cancelBtn = document.getElementById("nav-cancel")
+const hoverLink = document.getElementsByClassName("hover-link")
 
         showMenuBtn.addEventListener("click", () => {
-            navLinks.classList.toggle("show-links");
+            navLinks.style.top = "0%";
+            cancelBtn.style.display = "block";
         });
+
+
+        cancelBtn.addEventListener("click", () =>{
+            navLinks.style.top = "-100%";
+            cancelBtn.style.display = "none";
+        })
+
+        hoverLink.addEventListener("click", () =>{
+            navLinks.style.top = "-100%";
+            cancelBtn.style.display = "none";
+        })
